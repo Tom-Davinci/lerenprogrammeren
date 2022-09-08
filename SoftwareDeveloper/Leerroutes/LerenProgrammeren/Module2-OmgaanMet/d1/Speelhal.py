ff1dac30a303ba607f61f0ae4d179d9da17811f1
Tom-Davinci <99066335@mydavinci.nl>
2022-09-08 09:28:16 +0200

    opdrachten

---
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module2-OmgaanMet/d1/Speelhal.py
@@ -0,0 +1,6 @@
+mensen = 4
+minuten = 45
+entree = 7.45 * mensen
+vr = ( (45 /5 ) * 0.37) * mensen
+kost = round(entree + vr, 3)
+print("Dit geweldige dagje-uit met", mensen, "mensen in de Speelhal met", minuten, "minuten VR kost je maar:", kost, "euro")
---
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module2-OmgaanMet/d1/feestlunch.py
@@ -0,0 +1,10 @@
+acroi = 17
+astok = 2
+akort = 3
+
+croi = acroi * 0.39
+stok = astok * 2.78
+kort = akort * 0.5
+kost = croi + stok - kort
+
+print("De feestlunch kost je bij de bakker", kost, "euro voor de", acroi, "croissantjes en de", astok, "stokbroden als de", akort, "kortingsbonnen nog geldig zijn!")
