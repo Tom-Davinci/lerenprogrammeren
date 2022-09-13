a4e9aa0e0a872a172fcf2cc50d7b723b7ecd96a9
Tom-Davinci <99066335@mydavinci.nl>
2022-09-13 11:31:34 +0200

    KUT OPDRACT

---
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module2-OmgaanMet/d3/123.py
@@ -0,0 +1,6 @@
+while True:
+    kaas = input("kies x/y/z: ")
+    if kaas in ["x","y","z"]:
+        break
+
+print("goed gedaan!")
---
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module2-OmgaanMet/d4/bugs.py
@@ -0,0 +1,46 @@
+import random
+
+name = input('Wat is jouw naam? ')
+print('Hallo', name)
+seizoen = name
+
+favoriteSeason = input('Wat is jouw favorite seizoen {}? A) Lente, B) Zomer, C) Herfst of D) Winter '.format(name) )
+answer = favoriteSeason.lower()
+
+if answer == 'a':
+    print("Ik hou ook van de lente!")
+elif answer == 'b':
+    print("De zomer is voor mij te warm.")
+elif answer == 'c':
+    print("Mooi he, al die blaadjes die dan van de boom vallen.")
+elif answer == 'd':
+    print("Is de winter niet erg koud?")
+else:
+    print("Die ken ik niet...")
+
+favoriteColor = input('En wat is je favoriete kleur? ') 
+trueOrFalse = random.randint(0,1)
+
+if trueOrFalse == 1:
+    trueOrFalse = True
+else:
+    trueOrFalse = False
+
+if trueOrFalse == True:
+    print('Ik vind dat ook een mooie kleur!')
+elif trueOrFalse == False:
+    print('TBH, ik hou niet zo van {}...'.format(favoriteColor))
+
+num1 = random.randint(1,10)
+num2 = random.randint(5,15)
+
+checkawnser = int( num1 + num2)
+
+try:
+    number = int( input('En weet jij wat {} + {}, is? '.format(num1, num2) ) )
+    if number == checkawnser:
+        print('Dat is juist')
+    else:
+        print('Nee dat klopt niet {}'.format(name) )
+except:
+    print('Dat is geen nummer!')
