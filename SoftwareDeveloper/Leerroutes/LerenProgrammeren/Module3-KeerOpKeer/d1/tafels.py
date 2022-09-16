0c321de003e5a6213b306098a086646fc605b839
Tom-Davinci <99066335@mydavinci.nl>
2022-09-16 15:09:23 +0200

    tafels.py

---
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module3-KeerOpKeer/d1/tafels.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        x = int (input("Welk getal wilt u de tafels van? "))
+        break
+    except:
+        print("voer een heel getal in.")
+
+for i in range (1,11):
+    print(i * x)
+    i += 1
