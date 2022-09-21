e705b7d62f15cfbf6daf082d4b5ef23dc26eb9e8
Tom-Davinci <99066335@mydavinci.nl>
2022-09-21 13:07:40 +0200

    KNAKER

---
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module3-KeerOpKeer/d2/whiletijd.py
@@ -0,0 +1,12 @@
+tijd = 1
+ampm = 0
+
+while tijd < 13:
+    if ampm == 0:
+        print(f"Het is: {tijd}AM")
+    elif ampm == 1:
+        print(f"Het is: {tijd}PM")
+    if tijd == 12 and ampm == 0:
+        tijd = 1
+        ampm = 1
+    tijd += 1
