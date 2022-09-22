31f2e5a628931be320402f4af4307451fe6113f7
Tom-Davinci <99066335@mydavinci.nl>
2022-09-22 10:27:27 +0200

    omdat t moet

--- SoftwareDeveloper/Leerroutes/LerenProgrammeren/Challenges/smartphone.py
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Challenges/smartphone.py
@@ -12,21 +12,49 @@ while True:
     except:
         print("Geldig bedrag invoeren")
 
-if iphone > samsung:
+while True:
+    try:
+        zenfone = round( float( input("Prijs van de Zenfone: ") ), 2)
+        break
+    except:
+        print("Geldig bedrag invoeren")
+
+if iphone > samsung and iphone > zenfone:
     print(f"De iPhone is het duurst, de telefoon kost: {iphone}")
-    print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung}")
-elif samsung > iphone:
+    if samsung > zenfone:
+        print(f"De Samsung zit in het midden, de telefoon kost: {samsung}")
+        print(f"De Zenfone is het goedkoopst, de telefoon kost: {zenfone}")
+    else:
+        print(f"De Zenfone zit in het midden, de telefoon kost: {zenfone}")
+        print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung}")
+elif samsung > iphone and samsung > zenfone:
     print(f"De Samsung is het duurst, de telefoon kost: {samsung}")
-    print(f"De iPhone is het goedkoopst, de telefoon kost: {iphone}")
+    if iphone > zenfone:
+        print(f"De iPhone zit in het midden, de telefoon kost: {iphone}")
+        print(f"De Zenfone is het goedkoopst, de telefoon kost: {zenfone}")
+    else:
+        print(f"De Zenfone zit in het midden, de telefoon kost: {zenfone}")
+        print(f"De iPhone is het goedkoopst, de telefoon kost: {iphone}")       
+elif zenfone > samsung and zenfone > iphone:
+    print(f"De Zenfone is het duurst, de telefoon kost: {zenfone}")
+    if samsung > iphone:
+        print(f"De Samsung zit in het midden, de telefoon kost: {samsung}")
+        print(f"De iPhone is het goedkoopst, de telefoon kost: {iphone}")
+    else:
+        print(f"De iPhone zit in het midden, de telefoon kost: {iphone}")
+        print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung}")
 else:
     print("De telefoons zijn even duur, kies de iPhone")
     quit()
+# kon de berekning van de laatste uitbreiding niet bedenken
+# maar geen verrassingen natuurlijk :)
 
-if samsung > 900 and iphone > 900:
-    print("Koop geen van beide telefoons ze zijn te duur!")
+if samsung > 900 and iphone > 900 and zenfone > 900:
+    print("Koop geen van de telefoons ze zijn te duur!")
     quit()
 
 advies = round( iphone - samsung, 2)
+
 if advies < 0:
     advies = samsung - iphone
     print(f"Het advies is dus de iPhone te kopen. Deze is namelijk {advies} goedkoper dan de Samsung.")
