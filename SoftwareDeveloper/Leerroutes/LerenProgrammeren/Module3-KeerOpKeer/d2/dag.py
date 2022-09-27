5710294ecb8e5da71e130831e1cc7d14a594a688
Tom-Davinci <99066335@mydavinci.nl>
2022-09-27 09:57:43 +0200

    vieze gore kut opdracht

--- SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module3-KeerOpKeer/d2/dag.py
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module3-KeerOpKeer/d2/dag.py
@@ -1,6 +1,15 @@
+dayarr = ["ma","di","wo","do","vr","za","zo"]
+
 while True:
     day = input("Voer hier de dag in: ").lower()
-    if day in ["ma","di","wo","do","vr","za","zo"]:
+    if day in dayarr:
         break
     else:
         print("Afkorting van de dag invoeren [ma, di, wo, enz.].")
+
+index = 0
+while True:
+    print(dayarr[index])
+    if dayarr[index] == day:
+        break
+    index += 1
