e0eb6238c80576a8967bda6b3d165429fc21e2e1
Tom-Davinci <99066335@mydavinci.nl>
2022-09-28 10:32:50 +0200

    coins

---
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module3-KeerOpKeer/d2/coins/coins.py
@@ -0,0 +1,55 @@
+# name of student: Tom 
+# number of student: 99066335
+# purpose of program: Wisselgeld berekenen
+
+toPay = int(float(input('Amount to pay: '))* 100) # Maakt wisselgeld naar een heel getal
+paid = int(float(input('Paid amount: ')) * 100) # Doet het zelfde als boven
+change = paid - toPay # maakt de variabele "change" het aantal te veel betaald
+
+if change > 0: # Zorgt ervoor, dat er alleen wat gebeurt als er daadwerkelijk geld gewisseld moet worden.
+  coinValue = 50 # zorgt voor de initele grootte van kleingeld terug gegeven, dus 50 cent
+  
+  while change > 0 or coinValue > 0: # doet alleen wat als er nog wisselgeld is te geven, en er nog munten zijn om in te vullen
+    nrCoins = change // coinValue # berekent het aantal coins nodig, als het er niet inpast veranderd het, t aantal coins
+    nrCoinsReturned = 0
+
+    if nrCoins > 0: # Doet alleen wat als er coins gegeven kunnen worden
+      print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!' ) # Laat de gebruiker zien hoeveel coins er maximaal ge returned kunnen worden
+      nrCoinsReturned = int(input('How many coins of ' + str(coinValue) +  ' cents did you return? ')) #Vraagt de gebruiker hoeveel coins er gereturned zijn
+      change -= nrCoinsReturned * coinValue #Haalt het aantal coins gereturned van change af
+
+# comment on code below: Zorgt ervoor dat het kleingeld gevraagd steeds kleiner word
+    if coinValue == 50:
+      nr50Returned = nrCoinsReturned
+      coinValue = 20
+    elif coinValue == 20:
+      nr20Returned = nrCoinsReturned
+      coinValue = 10
+    elif coinValue == 10:
+      nr10Returned = nrCoinsReturned
+      coinValue = 5
+    elif coinValue == 5:
+      nr5Returned = nrCoinsReturned
+      coinValue = 3
+    elif coinValue == 3:
+      nr3Returned = nrCoinsReturned
+      coinValue = 2
+    elif coinValue == 2:
+      nr2Returned = nrCoinsReturned
+      coinValue = 1
+    else:
+      nr1Returned = nrCoinsReturned
+      coinValue = 0
+
+coinarr = [50,20,10,5,3,2,1]
+returnarr = [nr50Returned, nr20Returned, nr10Returned, nr5Returned, nr3Returned, nr2Returned, nr1Returned]
+index = 0
+while index < 7:
+  if returnarr[index] > 0:
+    print(f"Amount of {coinarr[index]} coins returned: {returnarr[index]}")
+  index += 1
+
+if change > 0: # Als er nog change over is print het dat.
+  print('Change not returned: ', str(change) + ' cents')
+else:
+  print('done')
