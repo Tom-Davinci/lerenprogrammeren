8a24577e4e803fbfcbc92858111d8a045cfa7089
Tom-Davinci <99066335@mydavinci.nl>
2022-10-07 13:41:41 +0200

    ik monkey

--- SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module4-LijstjesEnz/d1/mmm.py
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module4-LijstjesEnz/d1/mmm.py
@@ -15,11 +15,11 @@ for i in range(0, aantal):
     rnd = random.randint(0, 3)
     if rnd == 0:
         mmlist.append(kleuren[0])
-    if rnd == 1:
+    elif rnd == 1:
         mmlist.append(kleuren[1])
-    if rnd == 2:
+    elif rnd == 2:
         mmlist.append(kleuren[2])
-    if rnd == 3:
+    elif rnd == 3:
         mmlist.append(kleuren[3])
 
 print("Zak van M&M's: ")
