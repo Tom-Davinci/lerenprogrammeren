c464345b69e91320180d780187c5b1807376b383
Tom-Davinci <99066335@mydavinci.nl>
2022-12-08 11:25:13 +0100

    wk deel 6

--- SoftwareDeveloper/Leerroutes/LerenProgrammeren/Challenges/wk_challenge.py
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Challenges/wk_challenge.py
@@ -1,4 +1,5 @@
 GEWONNEN = 3
+GELIJKSPEL = 2
 
 def intinput():
     while True:
@@ -13,7 +14,7 @@ while True:
     land = input("Voer hier een land in\n").upper()
     if land not in groepA:
         groepA.append(land)
-        if len(groepA) >= 3:
+        if len(groepA) >= 4:
             break
     else:
         print("Nieuw land invoeren!")
@@ -30,10 +31,17 @@ rondes = 1
 t1 = -1
 t2 = 0
 
-
-while rondes < 3:
-    t1 += 1
-    t2 += 1
+while rondes < 7:
+    if rondes == 4:
+        t1 -= 2
+    elif rondes == 5:
+        t1 += 1
+    elif rondes == 6:
+        t1 -= 1
+        t2 -= 1
+    else:
+        t1 += 1
+        t2 += 1
 
     print(f"Wedstrijd {rondes} score {groepA[t1]} - {groepA[t2]}")
     print(f"Score {groepA[t1]}")
@@ -46,11 +54,14 @@ while rondes < 3:
         teams[t1]["punten"] += GEWONNEN
         teams[t1]["doelsaldo"] += doelsaldo
         teams[t2]["doelsaldo"] -= doelsaldo
-    else:
+    elif score2 > score1:
         doelsaldo = score2 - score1
         teams[t2]["punten"] += GEWONNEN
         teams[t2]["doelsaldo"] += doelsaldo
         teams[t1]["doelsaldo"] -= doelsaldo
+    else:
+        teams[t1]["punten"] += GELIJKSPEL
+        teams[t2]["punten"] += GELIJKSPEL
 
     print(f"Wedstrijd {groepA[t1]} - {groepA[t2]} eindigde in {score1} - {score2}")
     rondes += 1
