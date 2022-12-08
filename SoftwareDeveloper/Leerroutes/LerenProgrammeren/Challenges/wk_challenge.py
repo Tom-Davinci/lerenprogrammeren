528278b97d6ac32502335af2d7b2cda27df8b850
Tom-Davinci <99066335@mydavinci.nl>
2022-12-08 10:35:29 +0100

    wk deel 3

--- SoftwareDeveloper/Leerroutes/LerenProgrammeren/Challenges/wk_challenge.py
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Challenges/wk_challenge.py
@@ -1,5 +1,4 @@
 GEWONNEN = 3
-VERLOREN = 0
 
 def intinput():
     while True:
@@ -11,7 +10,7 @@ def intinput():
 
 groepA = []
 while True:
-    land = input("Voer hier een land in\n").lower()
+    land = input("Voer hier een land in\n").upper()
     if land not in groepA:
         groepA.append(land)
         if len(groepA) >= 3:
@@ -19,8 +18,38 @@ while True:
     else:
         print("Nieuw land invoeren!")
 
+teams = []
+for x in range( len(groepA)):
+    teams.append({
+        "name" : groepA[x],
+        "punten" : 0,
+        "doelsaldo" : 0
+        })
+
 print(f"Wedstrijd 1 score {groepA[0]} - {groepA[1]}")
 print(f"Score {groepA[0]}")
 score1 = intinput()
 print(f"Score {groepA[1]}")
 score2 = intinput()
+
+t1 = 0
+t2 = 1
+
+if score1 > score2:
+    doelsaldo = score1 - score2
+    teams[t1]["punten"] += GEWONNEN
+    teams[t1]["doelsaldo"] += doelsaldo
+    teams[t2]["doelsaldo"] -= doelsaldo
+else:
+    doelsaldo = score2 - score1
+    teams[t2]["punten"] += GEWONNEN
+    teams[t2]["doelsaldo"] += doelsaldo
+    teams[t1]["doelsaldo"] -= doelsaldo
+
+print(f"Wedstrijd {groepA[0]} - {groepA[1]} eindigde in {score1} - {score2}")
+print("overzicht groep A")
+for i in range( len(groepA)):
+    naam = teams[i]["name"]
+    punten = teams[i]["punten"]
+    doelsaldo = teams[i]["doelsaldo"]
+    print(f"{naam}: punten {punten}; doelsaldo: {doelsaldo}")
