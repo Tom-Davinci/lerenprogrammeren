2c1afcb76576de6a2af0ffd01272e559d6959fac
Tom-Davinci <99066335@mydavinci.nl>
2022-12-08 11:31:11 +0100

    fibo2

--- SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module5-MeerFun/d1/fibonacci2.py
+++ SoftwareDeveloper/Leerroutes/LerenProgrammeren/Module5-MeerFun/d1/fibonacci2.py
@@ -1,11 +1,14 @@
-amount = int( input("Hoe veel nummers van de fibonacci rij wilt u?\n"))
+amount = int( input("Hoe veel nummers van de fibonacci rij wilt u: "))
 
 def fibonacci(n1, n2, amount):
     if amount > 0:
+        next_fibo = n1 + n2
         n1 = n2
-        n2 = n1 + n2
-        return fibonacci(n1, n2, amount - 1)
+        print(n1)
+        return fibonacci(n1, next_fibo, amount - 1)
     else:
         return "done"
 
+amount -= 1
+print(0)
 print( fibonacci(0, 1, amount))
