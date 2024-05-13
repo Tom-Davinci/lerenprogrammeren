3bc949e8b7f5d1d9fe1724aea138dd870bfc1fac
Tom-Davinci <99066335@mydavinci.nl>
2024-05-13 13:48:36 +0200

    buttons red-green 2

--- SoftwareDeveloper/Bovenbouw/Front-End/JavaScript/buttons_redgreen/scripts.js
+++ SoftwareDeveloper/Bovenbouw/Front-End/JavaScript/buttons_redgreen/scripts.js
@@ -7,6 +7,8 @@ function addElement(num){
     const newButton = document.createElement("BUTTON");
     const text = document.createTextNode(num);
 
+    newButton.setAttribute("id", "b" + num)
+    localStorage.setItem(newButton.id, "0")
     newButton.appendChild(text);
     newButton.onclick = function() {buttonClicked(newButton)};
     newButton.style.backgroundColor = "green";
@@ -18,5 +20,24 @@ function addElement(num){
 }
 
 function buttonClicked(button) {
+    setColour(button, localStorage.getItem(button.id))
+    let buttonCounter = localStorage.getItem(button.id);
+    buttonCounter = parseInt(buttonCounter) + 1;
+    localStorage.setItem(button.id, buttonCounter);
+}
+
+function setColour(button, num) {
     button.style.backgroundColor = "red";
+    if(num > 0) {
+        button.style.backgroundColor = "purple";
+    }
+    if(num > 1) {
+        button.style.backgroundColor = "blue";
+    }
+    if(num > 2) {
+        button.style.backgroundColor = "zwart";
+    }
+    if(num > 3) {
+        button.remove();
+    }
 }
